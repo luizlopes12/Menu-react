c60ef206da672de5af94d15e177bd661b20cdbc7
Luiz Lopes <luizlopes12@outlook.com>
2022-02-19 18:04:23 -0300

    Estilizando os components

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react';
-import {Container} from './style'
+import * as C from './style'
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <Container>
-        
-        </Container>
+        <C.Container>
+            <C.Header>
+                <C.Logo>Logo</C.Logo>
+                <C.Nav>
+                    <C.NavButton>
+                        Opa
+                    </C.NavButton>
+                    <C.NavList>
+                        <C.NavLink>Link 1</C.NavLink>
+                        <C.NavLink>Link 2</C.NavLink>
+                        <C.NavLink>Link 3</C.NavLink>
+                        <C.NavLink>Link 4</C.NavLink>
+                    </C.NavList>
+                </C.Nav>
+            </C.Header>
+
+        </C.Container>
     );
 }
 
--- src/header/style.js
+++ src/header/style.js
@@ -3,6 +3,41 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     background-color: aliceblue;
-    height: 20px;
+    height: 8vh;
     width: 100%;
-`;  
+`;
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 8vh;
+    width: 80%;
+    margin: auto;
+`;
+export const Logo = styled.header`
+
+`;
+export const Nav = styled.nav`
+
+`;
+export const NavButton = styled.button`
+    display: none;
+    @media(max-width: 768px) {
+        display: initial; 
+    }
+`;
+export const NavList = styled.ul`
+    list-style: none;
+    display: flex;
+    @media(max-width: 768px) {
+        position: absolute;
+        display: block;
+        width: 90vw;
+        height: 92vh;
+    }
+`;
+export const NavLink = styled.li`
+    padding: 10px 15px;
+    margin: 2px;
+    background-color: aqua;
+`;
