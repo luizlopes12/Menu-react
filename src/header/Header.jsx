6ab64e3c68b464a53d6784cd95dde305a379082b
Luiz Lopes <luizlopes12@outlook.com>
2022-02-20 00:40:10 -0300

    Tentando dnv essa bagaça

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,28 +1,29 @@
 import { useState } from 'react';
-import * as C from './style'
+import {
+    Header,
+    Nav,
+    NavList,
+    NavLink,
+    NavButton
+} from './style'
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false)
     const handleMenu = () =>{
         setMenuOpen(!menuOpen)
     }
     return (
-        <C.Container>
-            <C.Header>
-                <C.Logo>Logo</C.Logo>
-                <C.Nav>
-                    <C.NavButton onClick={handleMenu}>
-                        Opa
-                    </C.NavButton>
-                    <C.NavList active={menuOpen}>
-                        <C.NavLink>Link 1</C.NavLink>
-                        <C.NavLink>Link 2</C.NavLink>
-                        <C.NavLink>Link 3</C.NavLink>
-                        <C.NavLink>Link 4</C.NavLink>
-                    </C.NavList>
-                </C.Nav>
-            </C.Header>
+        <>
+            <Nav>
+                <NavList>
+                    <NavLink>Home</NavLink>
+                    <NavLink>Services</NavLink>
+                    <NavLink>Contact</NavLink>
+                    <NavLink>About us</NavLink>
+                </NavList>
+                    <NavButton>BTN</NavButton>
+            </Nav>
+        </>
 
-        </C.Container>
     );
 }
 
--- src/header/style.js
+++ src/header/style.js
@@ -1,49 +1,26 @@
 import styled from "styled-components";
 
+export const Header = styled.header`
 
-export const Container = styled.section`
-    background-color: aliceblue;
-    height: 8vh;
+`
+export const Nav = styled.nav`
+    position: fixed;
+    top: 0;
     width: 100%;
 `;
-export const Header = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 8vh;
-    width: 80%;
-    margin: auto;
-    overflow: hidden;
-`;
-export const Logo = styled.header`
+export const Nav = styled.nav`
+    position: fixed;
 
 `;
 export const Nav = styled.nav`
+    position: fixed;
 
 `;
-export const NavButton = styled.button`
-    display: none;
-    @media(max-width: 768px) {
-        display: initial; 
-    }
+export const Nav = styled.nav`
+    position: fixed;
+
 `;
-export const NavList = styled.ul`
-    list-style: none;
-    display: flex;
-    @media(max-width: 768px) {
-        position: absolute;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-around;
-        background-color: green;
-        width: 90vw;
-        height: 92vh;
-        transition: all .2s linear;
-        transform: translateX(${props => props.active? '0' : '100%'});
-    }
-`;
-export const NavLink = styled.li`
-    padding: 10px 15px;
-    margin: 2px;
-    background-color: aqua;
+export const Nav = styled.nav`
+    position: fixed;
+
 `;
